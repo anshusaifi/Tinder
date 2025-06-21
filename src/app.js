420012d32b0cd04355bea46c5d7e94a6d69e5673
anshusaifi <saifianash&@gmail.com>
2025-06-21 12:21:55 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+console.log("hii");
+const express = require('express')
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("hello from server")
+})
+app.listen(3000,()=>{
+    console.log("server is listening on port 3000");
+})
