6f8922e409d75e2967f3a96442a0570aa0d71fba
anshusaifi <saifianash&@gmail.com>
2025-07-06 00:37:12 +0530

    update the port

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const corsOptions = {
   origin: 'http://localhost:5173',    
   credentials: true                   
 };
-
+ const PORT = 7777;
 app.use(cors(corsOptions));
 app.use("/",authRouter)
 app.use("/",profileRouter)
@@ -25,8 +25,8 @@ connectDB()
 
   .then(() => {
     console.log("Database Connected Succesfully");
-    app.listen(3000, () => {
-      console.log("server is listening on port 3000");
+    app.listen(PORT, () => {
+      console.log("server is listening on port"+PORT);
     });
   })
   .catch((err) => {
