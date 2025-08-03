87cdc93488ac3db7dc1c45a4df3fa0cd42e6507a
anshusaifi <saifianash&@gmail.com>
2025-08-03 11:55:39 +0530

    change cors for socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -13,7 +13,7 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173", "http://13.201.85.123"],
       credentials: true,
     },
   });
